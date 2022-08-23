f916fd0f49850ccb30b471ce1c4d84d1b99f2b0c
ameya-6964 <belvalkar7@gmail.com>
2022-08-24 00:15:15 +0530

    First File Added

---
+++ Basic JavaScript/basicSyntax.js
@@ -0,0 +1,7 @@
+//Copy This Code And Open Browser Press F12 to Opwn Console And Paste This Code To see Output//
+
+
+var x = 50; //Declaration
+var y = 100;//Declaration
+var sum = x + y;
+document.write(sum); // Prints variable value
