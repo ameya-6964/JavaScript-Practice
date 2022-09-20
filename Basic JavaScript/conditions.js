1d26e373584ce1be0ab0106c2fbe0ea6940bdb07
ameya-6964 <belvalkar7@gmail.com>
2022-09-20 18:22:29 +0530

    #added Some More Javascript

---
+++ Basic JavaScript/conditions.js
@@ -0,0 +1,44 @@
+                                    // CONDITIONS AND OPERATORS
+
+let a = 10;
+let b = 20;
+  if(a > b)
+  {
+    console.log("A is Greater Then B");
+  }
+  else{
+    console.log("B is Greater Then A");
+  }                                  
+
+  /*   OUTPUT 
+  B is Greater Then A
+  */
+
+
+  let c = 20; // Number
+  let d = "20"; //String
+  if(d == b)
+  {
+    console.log("C and D are Equal");
+  }
+  else
+  {
+    console.log("C and D are Not Equal");
+  }     
+
+   /*   OUTPUT 
+      C and D are Equal (This is Because == Operator Does Not Check For Datatype To Avoid This Use ===)
+  */
+ 
+      let e = 20; // Number
+      let f = "20"; //String
+      if(f === e)
+      {
+        console.log("They Are Same");
+      }
+      else
+      {
+        console.log("E and F are Not Equal");
+      } 
+
+      
