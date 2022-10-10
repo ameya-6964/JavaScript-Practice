bf125ca7bb14ab54b0d3ea24ccc11735c4cf3639
ameya-6964 <belvalkar7@gmail.com>
2022-10-10 23:42:43 +0530

    Added Output and Solved src Issue

--- JavaScript Coursbook and Lecture Assignments/JavaScript  Coursebook Assignments/Assignment Number 1 (Tower Of Hanoi )/towerOfHanoi.js
+++ JavaScript Coursbook and Lecture Assignments/JavaScript  Coursebook Assignments/Assignment Number 1 (Tower Of Hanoi )/towerOfHanoi.js
@@ -1,7 +1,6 @@
 const towerOfHanoi= (n) => {
     let a = (Math.pow(2,n))-1;
     console.log("Minimum Amount Of Steps Required For " + n + " Number Of Disc's is " + a);
-    return a;
 };
 
 towerOfHanoi(1);
@@ -10,3 +9,13 @@ towerOfHanoi(3);
 towerOfHanoi(4);
 towerOfHanoi(5);
 towerOfHanoi(0);
+
+/* 
+* OUTPUT
+* Minimum Amount Of Steps Required For 1 Number Of Disc's is 1
+* Minimum Amount Of Steps Required For 2 Number Of Disc's is 3
+* Minimum Amount Of Steps Required For 3 Number Of Disc's is 7
+* Minimum Amount Of Steps Required For 4 Number Of Disc's is 15
+* Minimum Amount Of Steps Required For 5 Number Of Disc's is 31
+* Minimum Amount Of Steps Required For 0 Number Of Disc's is 0 
+ */
