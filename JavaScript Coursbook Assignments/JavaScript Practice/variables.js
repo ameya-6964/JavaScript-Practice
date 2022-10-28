29d3d57177a9fccd5c87ab02b8eaa0d0676e363c
ameya-6964 <belvalkar7@gmail.com>
2022-10-29 00:39:14 +0530

    Added Diffrent Types Of Variables

--- JavaScript Coursbook Assignments/JavaScript Practice/variables.js
+++ JavaScript Coursbook Assignments/JavaScript Practice/variables.js
@@ -1,1 +1,20 @@
 //Variables Are Used TO Store Information --> Container To Hold Data
+
+/* First Way using var Keyword */
+var variable1 = "hello i am First Variable From var Keyword";
+
+/* Second Way Using let Keyword (Most Prefered) */
+let variable2 = "hello i am Second Variable From let Keyword";
+
+/* Third Way Using const Keyword */
+const variable3 = "hello i am third Variable from const Keyword";
+
+console.log(variable1);
+console.log(variable2);
+console.log(variable3);
+
+/* Output
+ hello i am First Variable From var Keyword
+​ hello i am Second Variable From let Keyword
+​ hello i am third Variable from const Keyword
+*/
