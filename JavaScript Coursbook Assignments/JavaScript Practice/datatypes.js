e74e207afaf2b42a714e8f4976163d2231c4aba7
ameya-6964 <belvalkar7@gmail.com>
2022-10-29 00:49:32 +0530

    Added Datatypes

---
+++ JavaScript Coursbook Assignments/JavaScript Practice/datatypes.js
@@ -0,0 +1,40 @@
+
+// String Datatype
+var firstName = "Ameya";
+var lastName = 'Belvalkar';
+
+// Number Datatype
+var age= 24;
+
+// Boolean Datatype
+var isMarried = false;
+
+// Null Datatype
+var currentSalary = null;
+
+// Undefined Datatype
+var job = undefined;
+
+//Objects 
+var personalInformation =  {"name" : "Ameya","lastName":"Belvalkar","age":24,"isMarried":false};
+
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isMarried);
+console.log(currentSalary);
+console.log(job);
+console.log(personalInformation);
+
+
+/* 
+Ameya
+Belvalkar
+24
+false
+null
+undefined
+{name: 'Ameya', lastName: 'Belvalkar', age: 24, isMarried: false}
+
+*/
