d61700a8bc34b6cea481ba6b32d197a5412393fe
ameya-6964 <belvalkar7@gmail.com>
2022-10-31 21:51:17 +0530

    Done With Numbers

---
+++ JavaScript Coursbook Assignments/JavaScript Practice/Number.js
@@ -0,0 +1,25 @@
+// Not Need To Specify Whether Number is Whole Or Decimal It Is Specified By JavaScript Automatically At Runtime
+
+const wholeNumber=5;
+const decimalNumber=5.5;
+
+console.log(wholeNumber);
+console.log(decimalNumber);
+
+/* O/P
+5
+5.5
+*/
+
+
+const name="Ameya";
+const Number=5;
+
+const result = name/Number;
+
+console.log(result);
+
+/* OUTPUT
+ * Nan
+
+ */
