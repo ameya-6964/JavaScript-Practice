abfe231ad98a98729844feeb1a4d5295f58a8179
ameya-6964 <belvalkar7@gmail.com>
2022-11-04 22:32:30 +0530

    Added Conditional Folder

---
+++ JavaScript Practice/Logic And Flow Control/ifElseStatements.js
@@ -0,0 +1,14 @@
+const age1=17;
+if(age1>=18)
+{
+    console.log("You Are Allowed In The Club");
+}
+else
+{
+  console.log("Your Age Is Less Then 18 Years You Are Not Allowed");
+}
+
+
+/* OUTPUT 
+ Your Age Is Less Then 18 Years You Are Not Allowed
+*/
---
+++ JavaScript Practice/Logic And Flow Control/ifStatement.js
@@ -0,0 +1,11 @@
+const age1=28;
+if(age1>=18)
+{
+    console.log("You Are Allowed In The Club");
+}
+console.log("Welcome To The Club");
+
+/* OUTPUT 
+  You Are Allowed In The Club
+  Welcome To The Club
+*/
