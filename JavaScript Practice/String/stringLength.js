7bd473666b418a571167f767c18683b607ba4cc4
ameya-6964 <belvalkar7@gmail.com>
2022-12-18 20:22:28 +0530

    Added String Length and First and Last Letter

---
+++ JavaScript Practice/String/stringLength.js
@@ -0,0 +1,13 @@
+const username = `Ameya`;
+let stringLength = username.length;
+console.log(stringLength);
+//! OUTPUT - 5
+
+const firstLetter = username[0];
+console.log(firstLetter);
+// !OUTPUT - A  because A[0] m[1] e[2] y[3] a[4]
+
+const lastLetter = username[username.length-1]
+console.log(lastLetter);
+// !OUTPUT - a because A[0] m[1] e[2] y[3] a[4], Because Length is 5 Therefore 5-1 = [4] and at [4] we have 'a'
+
