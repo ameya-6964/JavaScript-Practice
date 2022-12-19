c14b0a3029cd2eee79479beefc5f266d569466c4
ameya-6964 <belvalkar7@gmail.com>
2022-12-19 22:10:05 +0530

    Added String Case Methods

---
+++ JavaScript Practice/String/stringCase.js
@@ -0,0 +1,18 @@
+// Upper Case And Lower CAse Letters
+
+const mixedCase="Hello How Are You???";
+
+const lowerCase= mixedCase.toLowerCase();
+console.log(lowerCase); 
+
+/*  OUTPUT
+ hello how are you??? 
+ */
+
+const upperCase= mixedCase.toUpperCase();
+console.log(upperCase); 
+
+/*  OUTPUT
+HELLO HOW ARE YOU???
+*/
+
