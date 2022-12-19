a5a406ecc9bd5f5b8943de37efe1e3866ae2d095
ameya-6964 <belvalkar7@gmail.com>
2022-12-19 22:26:00 +0530

    Added String Searching

---
+++ JavaScript Practice/String/stringSearching.js
@@ -0,0 +1,16 @@
+const userName = "Hello User Name Is Ameya and Ameya Is JavaScript Developer"
+
+const firstIndex = userName.indexOf("Ameya"); 
+const lastIndex = userName.lastIndexOf("Ameya"); 
+console.log(firstIndex); //19
+console.log(lastIndex); //23
+
+const includes = userName.includes("User");
+console.log(includes); //true
+
+
+const starts = userName.startsWith("H");
+const ends = userName.endsWith("r");
+console.log(starts); //true
+console.log(ends); //true
+
