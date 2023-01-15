08df7691f0d80019b5ec3769a91d59727c94d470
ameya-6964 <belvalkar7@gmail.com>
2023-01-16 01:46:47 +0530

    Mistakes

---
+++ JavaScript Practice/Arrays/ArrayIncludes.js
@@ -0,0 +1,12 @@
+const bookshelf = [
+  "Moby Dick",
+  "Little Women",
+  "The Great Gatsby",
+  "Pride And Prejudice",
+];
+
+if (bookshelf.includes("Moby Dick") === true) {
+  console.log("the book you were looking for was found");
+} else {
+  console.log("couldn't find the book, sorry. :c");
+}
