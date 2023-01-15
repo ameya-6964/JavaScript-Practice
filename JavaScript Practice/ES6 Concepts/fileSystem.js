632c91cae6c269e999633d06afe8f2109e7ba37f
ameya-6964 <belvalkar7@gmail.com>
2023-01-16 01:16:41 +0530

    Added Comments In My File System

--- JavaScript Practice/ES6 Concepts/fileSystem.js
+++ JavaScript Practice/ES6 Concepts/fileSystem.js
@@ -1,3 +1,15 @@
+/*                                    Node File System Module  
+The Node.js file system module allows you to work with the file system on your computer.
+
+Common use for the File System module:
+Read files
+Create files
+Update files
+Delete files
+Rename files 
+
+*/
+
 const fs = require("fs");
 
 fs.readFile("demo.txt", "utf-8", (err, data) => {
