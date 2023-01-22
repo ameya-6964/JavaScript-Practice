9efa20c0b71c6cc8acfe676769457b23d0f7f8d6
ameya-6964 <belvalkar7@gmail.com>
2023-01-22 17:37:31 +0530

    Testing Average Function

--- JavaScript Practice/functions/functions.js
+++ JavaScript Practice/functions/functions.js
@@ -5,3 +5,7 @@ const average = (a, b, c) => {
 const average1 = average(1, 2, 3);
 const average2 = average(2, 3, 5);
 const average3 = average(6, 7, 223);
+
+console.log(average1);
+console.log(average2);
+console.log(average3);
