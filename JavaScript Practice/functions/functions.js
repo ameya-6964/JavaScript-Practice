3a79f57831ce2633a3d66a9aacee9542a12911e6
ameya-6964 <belvalkar7@gmail.com>
2023-01-23 17:44:56 +0530

    Added Comments

--- JavaScript Practice/functions/functions.js
+++ JavaScript Practice/functions/functions.js
@@ -6,12 +6,12 @@ const average1 = average(1, 2, 3);
 const average2 = average(2, 3, 5);
 const average3 = average(6, 7, 223);
 
-console.log(average1);
-console.log(average2);
-console.log(average3);
+console.log(average1); // 4
+console.log(average2); //6.66
+console.log(average3); //87.333
 
 const getSquare = (number) => number * number; // FUNCTION EXPRESSION
 
 const getSquare1 = getSquare(5);
 
-console.log(getSquare1);
+console.log(getSquare1); //25
